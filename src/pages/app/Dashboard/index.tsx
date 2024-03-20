b7975fb838b589250212f0db13cf711acb108bbe
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2024-03-20 11:22:07 -0300

    refactor: testes para subir em produção

--- src/pages/app/Dashboard/index.tsx
+++ src/pages/app/Dashboard/index.tsx
@@ -1,22 +1,22 @@
-import { BarChart } from 'lucide-react'
+/* import { BarChart } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
-import { PopularProductsChart } from '../../../components/Charts/PopularProducts'
-import { RevenueChart } from '../../../components/Charts/Revenue'
-import { DayOrdersAccountCard } from '../../../components/Dashboard/Cards/DayOrdersAccount'
-import { MonthCanceledOrdersAccount } from '../../../components/Dashboard/Cards/MonthCanceledOrdersAccount'
-import { MonthOrdersAccountCard } from '../../../components/Dashboard/Cards/MonthOrdersAccount'
-import { MonthRevenueCard } from '../../../components/Dashboard/Cards/MonthRevenue'
+import { PopularProductsChart } from '@/components/Charts/PopularProducts'
+import { RevenueChart } from '@/components/Charts/Revenue'
+import { DayOrdersAccountCard } from '@/components/Dashboard/Cards/DayOrdersAccount'
+import { MonthCanceledOrdersAccount } from '@/components/Dashboard/Cards/MonthCanceledOrdersAccount'
+import { MonthOrdersAccountCard } from '@/components/Dashboard/Cards/MonthOrdersAccount'
+import { MonthRevenueCard } from '@/components/Dashboard/Cards/MonthRevenue'
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from '../../../components/ui/card'
+} from '@/components/ui/card' */
 
 export function Dashboard() {
-  return (
+  return <div></div> /* (
     <section className="flex flex-col gap-5 p-6">
       <Helmet title="Dashboard" />
       <h1 className="text-2xl font-semibold tracking-tight lg:text-4xl">
@@ -60,5 +60,5 @@ export function Dashboard() {
         </Card>
       </div>
     </section>
-  )
+  ) */
 }
